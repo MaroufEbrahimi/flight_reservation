9bfba2637edab2c2e82cfbe7ca98e9d27018a918
Marouf <maroufibrahimi7264@gmail.com>
2024-02-04 22:03:06 +0430

    Added Register Page & Displayed for The User

--- src/main/java/com/famous/flightreservation/FlightreservationApplication.java
+++ src/main/java/com/famous/flightreservation/FlightreservationApplication.java
@@ -8,7 +8,6 @@ public class FlightreservationApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(FlightreservationApplication.class, args);
-		System.out.println("Flight App");
 	}
 
 }
