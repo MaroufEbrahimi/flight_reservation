e7f17757fa6bb7205be156e8392db092bcac48ac
Marouf <maroufibrahimi7264@gmail.com>
2024-02-04 15:07:26 +0430

    initial commit

---
+++ src/test/java/com/famous/flightreservation/FlightreservationApplicationTests.java
@@ -0,0 +1,13 @@
+package com.famous.flightreservation;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FlightreservationApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
