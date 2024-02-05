d4f308878720f8f49aa44754601f83fd76c234b7
Marouf <maroufibrahimi7264@gmail.com>
2024-02-05 14:40:42 +0430

    save user from database and create Login page

--- src/main/java/com/famous/flightreservation/controllers/UserController.java
+++ src/main/java/com/famous/flightreservation/controllers/UserController.java
@@ -1,13 +1,27 @@
 package com.famous.flightreservation.controllers;
 
+import com.famous.flightreservation.entities.User;
+import com.famous.flightreservation.repos.UserRepository;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
 
 @Controller
 public class UserController {
 
+    @Autowired
+    private UserRepository userRepository;
+
     @RequestMapping("/showReg")
     public String showRegistrationPage() {
         return "login/registerUser";
+    }
+
+    @RequestMapping(value = "registerUser", method = RequestMethod.POST)
+    public String register(@ModelAttribute("user") User user) {
+        userRepository.save(user);
+        return "login/login";
     }
 }
