9356a5b70d2d4b825c7529e2c1ab6615c6b26935
Marouf <maroufibrahimi7264@gmail.com>
2024-02-07 14:25:03 +0430

    Create Reservation Request and Controller

--- src/main/java/com/famous/flightreservation/controllers/ReservationController.java
+++ src/main/java/com/famous/flightreservation/controllers/ReservationController.java
@@ -1,11 +1,13 @@
 package com.famous.flightreservation.controllers;
 
+import com.famous.flightreservation.dto.ReservationRequest;
 import com.famous.flightreservation.entities.Flight;
 import com.famous.flightreservation.repos.FlightRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.ModelMap;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RequestParam;
 
 @Controller
@@ -19,5 +21,10 @@ public class ReservationController {
         Flight flight = flightRepository.findById(flightId).orElse(null);
         modelMap.addAttribute("flight", flight);
         return "completeReservation";
+    }
+
+    @RequestMapping(value = "/completeReservation", method = RequestMethod.POST)
+    public String completeReservation(ReservationRequest request) {
+        return null;
     }
 }
