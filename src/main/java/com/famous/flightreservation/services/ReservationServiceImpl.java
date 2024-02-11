254701b03215ae43740e4d65684285b37f361782
Marouf <maroufibrahimi7264@gmail.com>
2024-02-11 19:17:17 +0430

    Added LogBack file

--- src/main/java/com/famous/flightreservation/services/ReservationServiceImpl.java
+++ src/main/java/com/famous/flightreservation/services/ReservationServiceImpl.java
@@ -1,5 +1,6 @@
 package com.famous.flightreservation.services;
 
+import com.famous.flightreservation.controllers.ReservationController;
 import com.famous.flightreservation.dto.ReservationRequest;
 import com.famous.flightreservation.entities.Flight;
 import com.famous.flightreservation.entities.Passenger;
@@ -7,6 +8,8 @@ import com.famous.flightreservation.entities.Reservation;
 import com.famous.flightreservation.repos.FlightRepository;
 import com.famous.flightreservation.repos.PassengerRepository;
 import com.famous.flightreservation.repos.ReservationRepository;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
@@ -16,6 +19,9 @@ public class ReservationServiceImpl implements ReservationService {
     @Autowired
     FlightRepository flightRepository;
 
+    private static final Logger LOGGER = LoggerFactory.getLogger(ReservationServiceImpl.class);
+
+
     @Autowired
     PassengerRepository passengerRepository;
 
@@ -24,8 +30,10 @@ public class ReservationServiceImpl implements ReservationService {
 
     @Override
     public Reservation bookFlight(ReservationRequest request) {
+        LOGGER.info("Inside bookFlight()");
         // make Payment
         Long flightId = request.getFlightId();
+        LOGGER.info("Fetching flight for flight ID: " + flightId);
         Flight flight = flightRepository.findById(flightId).orElse(null);
 
         // Passenger
@@ -34,6 +42,7 @@ public class ReservationServiceImpl implements ReservationService {
         passenger.setLastName(request.getPassengerLastName());
         passenger.setEmail(request.getPassengerEmail());
         passenger.setPhone(request.getPassengerPhone());
+        LOGGER.info("Saving the passenger: " + passenger);
         Passenger savedPassenger = passengerRepository.save(passenger);
 
         // Reservation
@@ -41,6 +50,7 @@ public class ReservationServiceImpl implements ReservationService {
         reservation.setFlight(flight);
         reservation.setPassenger(savedPassenger);
         reservation.setCheckedIn(false);
+        LOGGER.info("Saving the reservation: " + reservation);
         Reservation savedReservation = reservationRepository.save(reservation);
 
         return savedReservation;
